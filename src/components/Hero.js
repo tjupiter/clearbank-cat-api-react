500dafcd5dfcc167da308c589b6c3ba632ae4a75
Istvan Pitju Balogh <bpityu@gmail.com>
2022-07-04 22:00:04 +0100

    fixed gsap animation

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,58 @@
-import React from 'react'
-import './Hero.css'
+import { useRef, useEffect} from 'react'
+import { gsap } from 'gsap';
+// import { CSSPlugin } from 'gsap/CSSPlugin';
+import './Hero.css';
 
+
+import runningCatGif from '../images/running-cat.gif'
 import downChevron from '../images/circle-chevron-down-solid.svg'
 
 function Hero() {
+
+const runningCat = useRef();
+const maingBackground = useRef();
+const headerText = useRef();
+const paragraphText = useRef();
+// gsap.registerPlugin(CSSPlugin)
+useEffect(() => {
+  // const timeLine = gsap.timeline()
+
+  // timeLine.from(runningCat, {duration: 4, transform: "translateX(-200%)"})
+  gsap.fromTo('.cat-gif-container', {x: "-300%"}, {duration: 5.5, x: "300%"})
+  gsap.fromTo('.hero', { x: "-180%" }, { duration: 3.5, x: "0", delay: .8 })
+  gsap.fromTo('.hero-h2, .hero--bottom-text', { opacity: 0 }, { duration: 2.5, opacity: 1, delay: 2
+})
+  gsap.to('.chevron', {rotation: 360, duration: .7, delay:4.5})
+  // gsap.fromTo('.hero--bottom-text', {})
+
+  // gsap.from('.hero-h2', {duration: 3, opacity: 0})
+
+  
+}, [])
+
+
   return (
-    <section className = "hero" >
-            <h2>Welcome to <br className="hero--br" />
-                <span className="hero--kittyland">KittyPicLand</span>
-            </h2>
-            <div className="hero--bottom-text">
-                <p>Scroll down for cute kitty pics</p>
-                
-                <p className="hero--chevron">
-                    <img src={downChevron} width="35px" alt=""/>
-                </p>
-            </div>
-    </section>
+    <div className="hero-container">
+      <div ref={runningCat} className="cat-gif-container">
+          <img 
+            src={runningCatGif} 
+            alt="running cat gif" className="catGif" />
+        </div>
+      <section className="hero" ref={maingBackground}>
+        <h2 ref={headerText} className="hero-h2">Welcome to <br className="hero--br" />
+          <span className="hero--kittyland">KittyPicLand</span>
+        </h2>
+        
+        <div className="hero--bottom-text" ref={paragraphText}>
+          <p>Scroll down for cute kitty pics</p>
+  
+          <p className="hero--chevron">
+            <img src={downChevron} width="25px" alt="" className="chevron"/>
+          </p>
+        </div>
+        {/* <div className="hero-background"></div> */}
+      </section>
+    </div>
   )
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,64 +1,100 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import './Main.css'
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function Main() {
 
   const [catData, setCatData] = React.useState([]);
+  const mainContainer = useRef(null)
+  const [isDataLoading, setIsDataLoading] = React.useState(true)
 
+  
+  // ===================
+  // FETCH DATA FROM API
+  // ===================
   useEffect(() => {
-
     // https://docs.thecatapi.com/authentication
     const fetchCatData = async () => {
-      console.log("fetchCatData is running")
       const res = await fetch('https://api.thecatapi.com/v1/images/search?limit=50', {
         headers: {
           'X-API-KEY': '24d14f66-0979-47e9-bbbb-a813b8223ae4'
         }
       }, [])
       const result = await res.json()
-      console.log(result)
-      setCatData(result)
-      // console.log(catData)
+      setCatData(result);
+      setIsDataLoading(false)
     }
     fetchCatData()
-    
   }, [])
 
-  const className = (i) => {
-  
+
+  // =============================
+  // GSAP animation for cat images
+  // =============================
+  useEffect(() => {
     
-    if (i % 4 === 0 && i % 6 !== 0) {
-      console.log(i)
+    gsap.config({
+      nullTargetWarn: false //disables console warning message
+    })
+
+    const element = mainContainer.current
+
+    gsap.set(element.querySelectorAll('.cat-image'), {opacity: 0, autoAlpha: 0})
+    gsap.registerPlugin(ScrollTrigger); 
+      ScrollTrigger.batch(element.querySelectorAll('.cat-image'), {
+        interval: 0.1,
+        batchMax: 3,
+        onEnter: batch => gsap.to(batch, { opacity: 1, autoAlpha: 1, stagger: 0.15, overwrite: true }),
+        onLeave: batch => gsap.set(batch, { opacity: 0, autoAlpha: 0, overwrite: true }),
+        onEnterBack: batch => gsap.to(batch, { autoAlpha: 1, stagger: 0.15, overwrite: true }),
+        onLeaveBack: batch => gsap.set(batch, { autoAlpha: 0, overwrite: true }),
+        markers: false,
+        start: "top 85%",
+        end: "bottom 40%"
+      });
+      ScrollTrigger.refresh()
+
+      // mobile view - there's a bug, animation not working. Some divs don't have the relevant class names, ie 'cat-container'
+
+  }, [catData])
+
+  // ====================
+  // ADD GRID CLASS NAMES
+  // ====================
+  const setClassName = (index) => {
+    if (index % 4 === 0 && index % 6 !== 0) {
       return "cat-container vertical"
-    } else if (i % 5 === 0) {
-      console.log(i)
+    } else if (index % 5 === 0) {
       return "cat-container horizontal"
-    } else if (i % 6 === 0) {
-      console.log(i)
+    } else if (index % 6 === 0) {
       return "cat-container bigbox"
+    } else {
+      return "cat-container"
     }
   }
 
-
-
   return (
-    
-  
-    <section className="main-cat-container">
-      {catData.map((cat, index) => 
-        <div 
-          key={cat.id}
-          className={className(index)}
-          >
-          <img 
-            src={cat.url}
-            alt="cat"
-            className="cat-image"
-            />
-        </div>  
-        )}
+    <>
+      {isDataLoading && 
+        <div className="loading-container">Loading cute kitty pics...</div>
+      }
 
-
-    </section>
+      <section className="main-cat-container" ref={mainContainer}>
+      {/* replace div with Card.js component */}
+        {catData.map((cat, index) => 
+          <div 
+            key={cat.id}
+            className={setClassName(index)}
+            >
+            <img 
+              src={cat.url}
+              alt="cat"
+              className="cat-image"
+              />
+          </div>  
+          )}
+      </section>
+    </>
   )
 }
